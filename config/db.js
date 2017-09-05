0f72e0f3b171515c5b6bcf7781cae0fd0c11b54b
nowz <1959.sami.1959@gmail.com>
2017-09-05 09:08:17 +0000

    new

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose=require("mongoose");
 var connection_string = '127.0.0.1:27017/task';
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+if(process.env.MONGODB_PASSWORD){
+  connection_string = process.env.MONGODB_USER + ":" +
+  process.env.MONGODB_PASSWORD + "@" +
+  "http://nodejs-mongo-persistent-taskmanager.a3c1.starter-us-west-1.openshiftapps.com" + ':' +
+  27017 + '/' +
+  process.env.MONGODB_DATABASE;
 }
 mongoose.connect(connection_string);
 mongoose.Promise=global.Promise;
