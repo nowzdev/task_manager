28331e53fbc064ac0339cc79dda4456e1e07313c
nowz <1959.sami.1959@gmail.com>
2017-09-05 08:18:42 +0000

    new

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 const mongoose=require("mongoose");
-console.log(process.env.MONGOLAB_URI)
-mongoose.connect(process.env.MONGOLAB_URI);
+var connection_string = '127.0.0.1:27017/task';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+mongoose.connect(connection_string);
 mongoose.Promise=global.Promise;
 module.exports={mongoose};
