10df3c31bfa79bdb2fc315bf605a1fdca3d4c177
nowz <1959.sami.1959@gmail.com>
2017-09-06 13:14:15 +0000

    new

--- config/db.js
+++ config/db.js
@@ -1,41 +1,12 @@
 const mongoose=require("mongoose");
 var connection_string = 'mongodb://127.0.0.1:27017/task';
 // if OPENSHIFT env variables are present, use the available connection info:
-var mongoURL=null;
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-  console.log(mongoURL)
-  mongoose.connect(mongoURL,{
+  mongoose.connect("mongodb://nowztask:taskpassword@ds127034.mlab.com:27034/task",{
     useMongoClient: true
     /* other options */
   });
   mongoose.Promise=global.Promise;
-};
+
 // if(process.env.MONGODB_PASSWORD){
 //   connection_string = "mongodb://"+process.env.MONGODB_USER + ":" +
 //   process.env.MONGODB_PASSWORD + "@" +
@@ -47,6 +18,5 @@ var initDb = function(callback) {
 //   useMongoClient: true
 //   /* other options */
 // });
-console.log(connection_string)
 mongoose.Promise=global.Promise;
 module.exports={mongoose};
